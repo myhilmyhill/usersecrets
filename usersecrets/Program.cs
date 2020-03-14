6bbffe64e81e43caa875aea096109fd943db63dd
myhilmyhill <44314051+myhilmyhill@users.noreply.github.com>
2020-03-15 02:19:32 +0900

    Add project files.

---
+++ usersecrets/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // It is required to set "User Secrets" to the project as following steps:
+        // 1. Right click on this project in Solution Explorer
+        // 2. Click "Manage User Secrets"
+        //   - Notes: May not displayed when "ASP.NET and web development" not installed
+        // 3. Add an item has "test" key to User Secrets then save
+
+        var configuration = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+            
+        Console.WriteLine(configuration["test"]);
+    }
+}
